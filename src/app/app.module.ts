70c866def8f71d7cb63f330b0c6649d0b53ef41f
Ole Voß <ole.voss@bertelsmann.de>
2021-06-11 14:09:22 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SlotComponent } from './modules/slot/slot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlotComponent
   ],
   imports: [
     BrowserModule
